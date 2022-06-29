d54365d0d480fb6afe9ea99f6c227d9c5f4a157d
OliveiraDCI <oliveira.sein.dci@gmail.com>
2022-06-29 12:20:57 +0200

    Initial commit - Exercise concluded.

--- solution.js
+++ solution.js
@@ -1,1 +1,30 @@
-// write your code in this file
+// 1
+console.log("This is an exercise in console logging");
+
+// 2
+const exercise = "This is the value";
+console.log(exercise);
+
+// 3
+const firstName = "Rafael";
+const lastName = "B Oliveira";
+console.log(firstName, lastName);
+
+// 4
+const city = "Hamburg";
+const job = "Programmer";
+const age = 36;
+console.log(firstName, lastName, city, job, age);
+
+// 5
+console.log(
+  firstName,
+  lastName +
+    " is a " +
+    age +
+    " years old " +
+    job +
+    " who lives in " +
+    city +
+    "."
+);
